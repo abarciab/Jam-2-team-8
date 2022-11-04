dbf8f1ab0400dc4dbbd7cf9500eccfa5311993e4
Koobysnack <97823937+Koobysnack@users.noreply.github.com>
2022-11-03 23:01:39 -0700

    Journal Transition fixed. Jankiness solves all

--- Assets/Final/Scripts/MapButtons.cs
+++ Assets/Final/Scripts/MapButtons.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class MapButtons : MonoBehaviour
 {
     public void goToRoom(string roomName) {
-        StartCoroutine(RoomManager.instance.startRoomTransition(roomName));
-        RoomManager.instance.journalButton.transform.GetChild(0).gameObject.SetActive(false);
-        JournalManager.instance.CloseJournal();
+        RoomManager.instance.transitionCoroutine(roomName);
+        RoomManager.instance.journalButton.GetComponent<JournalButton>().toggleJournal();
     }
 }
